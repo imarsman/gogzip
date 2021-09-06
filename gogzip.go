d6d09a82eb7458bac3549a894fb31de5f7e38a4f
Ian Marsman <ian@xmlteam.com>
2021-09-06 00:12:46 -0400

    More readying for trying to run

--- gogzip.go
+++ gogzip.go
@@ -262,32 +262,36 @@ func main() {
 		fmt.Println(paths)
 	}
 
-	// files := make([]*os.File, 0, len(paths))
-	for _, p := range goodPaths {
-		var fname string = p + ".gz"
-		file, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, colour(brightRed, err.Error()))
-			continue
+	// There are files to compress
+	if !stdoutFlag && len(goodPaths) > 0 {
+		// files := make([]*os.File, 0, len(paths))
+		for _, p := range goodPaths {
+			var fname string = p + ".gz"
+			file, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, colour(brightRed, err.Error()))
+				continue
+			}
+			fmt.Println(file.Name())
 		}
-		fmt.Println(file.Name())
 	}
 
 	if stdoutFlag && len(goodPaths) > 0 {
 		fmt.Fprintln(os.Stderr, colour(brightRed, errors.New("files specified along with stdout").Error()))
 		os.Exit(1)
-	}
+	} else if stdoutFlag {
 
-	// Use stdin if available, otherwise exit.
-	stat, _ := os.Stdin.Stat()
-	if (stat.Mode()&os.ModeCharDevice) == 0 && len(goodPaths) == 0 && stdoutFlag {
-		data, _, err := gZip(os.Stdin, level)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, colour(brightRed, err.Error()))
-			os.Exit(1)
+		// Use stdin if available, otherwise exit.
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) == 0 {
+			data, _, err := gZip(os.Stdin, level)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, colour(brightRed, err.Error()))
+				os.Exit(1)
+			}
+
+			reader := bytes.NewReader(data)
+			io.CopyBuffer(os.Stdout, reader, data)
 		}
-		// bb := new(bytes.Buffer)
-		reader := bytes.NewReader(data)
-		io.CopyBuffer(os.Stdout, reader, data)
 	}
 }
